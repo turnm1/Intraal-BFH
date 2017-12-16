97b4fe4efc64c74d5b573d4b20cedd4b96fed6ec
Mete <mete.turna@spf-consulting.ch>
2017-12-16 15:01:46 +0100

    SmartMe rest api

--- src/intraal/bt/config/connection/rest/DeviceRestService.java
+++ src/intraal/bt/config/connection/rest/DeviceRestService.java
@@ -5,18 +5,19 @@
  */
 package intraal.bt.config.connection.rest;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+
 import org.apache.commons.codec.binary.Base64;
-import sun.misc.BASE64Decoder;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.HttpRequest;
 
 /**
  *
@@ -24,7 +25,7 @@ import sun.misc.BASE64Decoder;
  */
 public class DeviceRestService {
 
-    String webPage = "https://smart-me.com:443";
+    String webPage = "https://smart-me.com";
     String name = "vnf1";
     String password = "f6PEz6QsTZnn";
     String authString = name + ":" + password;
@@ -76,22 +77,33 @@ public class DeviceRestService {
         return result;
     }
 
-    public void putMethode(String methode) throws MalformedURLException, IOException {
-        URLConnection connection = new URL(webPage + methode).openConnection();
-        connection.setRequestProperty("Accept-Charset", "UTF-8");
-        InputStream response = connection.getInputStream();
+    public void putMethode(String methode) throws MalformedURLException, IOException, UnirestException {
+//        URLConnection connection = new URL(webPage + methode).openConnection();
+//        connection.setRequestProperty("Accept-Charset", "UTF-8");
+//        InputStream response = connection.getInputStream();
+    	System.out.println(webPage + methode);
+        byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+        String authStringEnc = new String(authEncBytes);
+    	HttpRequest request = Unirest.put(webPage + methode)
+//    			.header("accept", "application/json")
+//    			.header("Accept-Charset", "UTF-8")
+    			.header("authorization", "basic " + authStringEnc)
+    			.queryString("switchState", "false");
+    	HttpResponse<String> jsonResponse = request.asString();
+    	System.out.println(jsonResponse.getBody().toString());
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, UnirestException {
 
         DeviceRestService drs = new DeviceRestService();
-        drs.checkConnection();
+//        drs.checkConnection();
         Device d = new Device();
 
 //       String JSON = drs.getMethode("/api/Devices/5b9b3fea-cc8d-45ad-92b3-9caf3be725bc");
 //        d.parsJsonToObjetc(JSON);
 //        System.out.println(d.getValueDate()+" "+d.getName()+" "+d.getSerial());
-        drs.putMethode("/api/Devices/5b9b3fea-cc8d-45ad-92b3-9caf3be725bc/false");
+//        drs.putMethode("/api/Devices/5b9b3fea-cc8d-45ad-92b3-9caf3be725bc?switchState=true");
+        drs.putMethode("/api/Devices/5b9b3fea-cc8d-45ad-92b3-9caf3be725bc");
 
     }
 }
